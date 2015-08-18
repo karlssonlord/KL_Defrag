0c9aa4205f219fbe1b237a483e19cbd3f33f5bfe
Robert Lord <robert@codepeak.se>
2015-08-18 22:53:49 +0200

    Setup everything except the optimization since we need to test the module for a while

--- app/code/local/KL/Defrag/Helper/Data.php
+++ app/code/local/KL/Defrag/Helper/Data.php
@@ -5,5 +5,17 @@
  */
 class KL_Defrag_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    /**
+     * Save data to the log
+     *
+     * @param $data
+     *
+     * @return $this
+     */
+    public function log($data)
+    {
+        Mage::log($data, null, 'kl_defrag.log', true);
+
+        return $this;
+    }
 }
-	 
--- app/code/local/KL/Defrag/Model/Cron.php
+++ app/code/local/KL/Defrag/Model/Cron.php
@@ -10,7 +10,69 @@ class KL_Defrag_Model_Cron
      */
     public function defrag()
     {
-        
-        //do something
+        /**
+         * Fetch database name
+         */
+        $databaseName = Mage::getConfig()->getResourceConnectionConfig('default_setup')->dbname;
+
+        /**
+         * Get our helper
+         */
+        $helper = Mage::helper('kldefrag');
+
+        /**
+         * Get the resource model
+         */
+        $resource = Mage::getSingleton('core/resource');
+
+        /**
+         * Retrieve the read connection
+         */
+        $readConnection = $resource->getConnection('core_read');
+
+        /**
+         * Retrieve the write connection
+         */
+        $writeConnection = $resource->getConnection('core_write');
+
+        /**
+         * Setup our magic query to fetch table fragmentation
+         */
+        $sqlQuery = "SELECT ENGINE, TABLE_SCHEMA, TABLE_NAME, Round(DATA_LENGTH/1024/1024) AS data_length, round(INDEX_LENGTH/1024/1024) AS index_length, round(DATA_FREE/ 1024/1024) AS data_free, round((data_free/(data_length+index_length))/100) AS fragmentation FROM information_schema.tables WHERE DATA_FREE > 0 AND TABLE_SCHEMA='" . $databaseName . "' ORDER BY fragmentation DESC";
+
+        /**
+         * Fetch fragmentation status
+         */
+        foreach ($readConnection->fetchAll($sqlQuery) as $dbRow) {
+
+            /**
+             * Act only if fragmentation is above 5
+             */
+            if ($dbRow['fragmentation'] >= 5) {
+
+                /**
+                 * Save a notice to the log
+                 */
+                $helper->log(
+                    sprintf(
+                        'Table "%s" has a fragmentation value of %s (%s mb free) and will be optimized',
+                        $dbRow['TABLE_NAME'],
+                        $dbRow['fragmentation'],
+                        $dbRow['data_free']
+                    )
+                );
+
+                /**
+                 * @todo act on table to optimize
+                 */
+                switch (strtolower($dbRow['ENGINE'])) {
+                    case 'innodb':
+                        break;
+                    case 'myisam':
+                        break;
+                }
+            }
+
+        }
     }
 }
