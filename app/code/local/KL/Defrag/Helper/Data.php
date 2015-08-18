28eb6ea22791c08b0fbc33bfa0c1587feb60e366
Robert Lord <robert@codepeak.se>
2015-08-18 22:39:13 +0200

    Initial module structure

---
+++ app/code/local/KL/Defrag/Helper/Data.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class KL_Defrag_Helper_Data
+ */
+class KL_Defrag_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
---
+++ app/code/local/KL/Defrag/Model/Cron.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class KL_Defrag_Model_Cron
+ */
+class KL_Defrag_Model_Cron
+{
+    /**
+     * Defrag tables
+     */
+    public function defrag()
+    {
+        
+        //do something
+    }
+}
